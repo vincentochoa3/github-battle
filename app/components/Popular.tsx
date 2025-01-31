ad1bd84816f81491d99869d72fa040d3dc65e09f
vincentochoa3 <vochoa893@gmail.com>
2025-01-31 12:17:24 -0800

    refactor ReposGrid as own component

--- app/components/Popular.tsx
+++ app/components/Popular.tsx
@@ -1,63 +1,9 @@
 import React from "react";
-import PropTypes from "prop-types";
 import { fetchPopularRepos, Repo } from "../utils/api";
-import {
-  FaUser,
-  FaStar,
-  FaCodeBranch,
-  FaExclamationTriangle,
-} from "react-icons/fa";
 import LanguagesNav, { Languages } from "./LanguagesNav";
-import Card from "./Card";
 import Loading from "./Loading";
-import Tooltip from "./Tooltip";
+import ReposGrid from "./ReposGrid";
 
-function ReposGrid({ repos }: { repos: Repo[] }) {
-  return (
-    <ul className="grid space-around">
-      {repos.map((repo, index) => {
-        const { name, owner, html_url, stargazers_count, forks, open_issues } =
-          repo;
-        const { login, avatar_url } = owner;
-        return (
-          <li key={html_url}>
-            <Card
-              header={`#${index + 1}`}
-              avatar={avatar_url}
-              href={html_url}
-              name={login}
-            >
-              <ul className="card-list">
-                <li>
-                  <Tooltip text="Github Username">
-                    <FaUser color="rgb(255, 191, 116)" size={22} />
-                    <a href={`https://github.com/${login}`}>{login}</a>
-                  </Tooltip>
-                </li>
-                <li>
-                  <FaStar color="rgb(255, 215, 0)" size={22} />
-                  {stargazers_count.toLocaleString()} stars
-                </li>
-                <li>
-                  <FaCodeBranch color="rgb(129, 195, 245)" size={22} />
-                  {forks.toLocaleString()} forks
-                </li>
-                <li>
-                  <FaExclamationTriangle color="rgb(241, 138, 147)" size={22} />
-                  {open_issues.toLocaleString()} open
-                </li>
-              </ul>
-            </Card>
-          </li>
-        );
-      })}
-    </ul>
-  );
-}
-
-ReposGrid.propTypes = {
-  repos: PropTypes.array.isRequired,
-};
 interface PopularState extends Partial<Record<Languages, Repo[]>> {
   error: null | string;
 }

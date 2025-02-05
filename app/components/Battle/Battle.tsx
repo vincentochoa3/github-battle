49ac405dd23c404f24ba016de4d8e0e03bc3536c
vincentochoa3 <vochoa893@gmail.com>
2025-02-05 13:37:10 -0800

    refactor PlayerPreview as own component

--- app/components/Battle/Battle.tsx
+++ app/components/Battle/Battle.tsx
@@ -1,47 +1,8 @@
 import React from "react";
-import { FaTimesCircle } from "react-icons/fa";
-import PropTypes from "prop-types";
-import ThemeContext from "../../contexts/theme";
 import { Link } from "react-router-dom";
 import Instructions from "./Instructions";
 import PlayerInput from "./PlayerInput";
-
-function PlayerPreview({
-  username,
-  onReset,
-  label,
-}: {
-  label: string;
-  username: string;
-  onReset: () => void;
-}) {
-  const theme = React.useContext(ThemeContext);
-  return (
-    <div className="column player">
-      <h3 className="player-label">{label}</h3>
-      <div className={`row bg-${theme}`}>
-        <div className="player-info">
-          <img
-            className="avatar-small"
-            src={`https://github.com/${username}.png?size=200`}
-          />
-          <a href={`https://github.com/${username}`} className="link">
-            {username}
-          </a>
-        </div>
-        <button className="btn-clear flex-center" onClick={onReset}>
-          <FaTimesCircle color="rgb(194, 57, 42)" size={26} />
-        </button>
-      </div>
-    </div>
-  );
-}
-
-PlayerPreview.propTypes = {
-  username: PropTypes.string.isRequired,
-  onReset: PropTypes.func.isRequired,
-  label: PropTypes.string.isRequired,
-};
+import PlayerPreview from "./PlayerPreview";
 
 export default function Battle() {
   const [playerOne, setPlayerOne] = React.useState("");

2299c7eff515e14f0a3332c7fd25c288a38c0efb
vincentochoa3 <vochoa893@gmail.com>
2025-02-11 15:24:38 -0800

    remove unused styles object from Results component

--- app/components/Results/Results.tsx
+++ app/components/Results/Results.tsx
@@ -7,28 +7,6 @@ import { Link } from "react-router-dom";
 import ProfileList from "./ProfileList";
 import { BattleAction, BattleStateTypes } from "../../types/types";
 
-const styles = {
-  container: {
-    position: "relative",
-    display: "flex",
-  },
-  tooltip: {
-    boxSizing: "border-box",
-    position: "absolute",
-    width: "160px",
-    bottom: "100%",
-    left: "50%",
-    marginLeft: "-80px",
-    borderRadius: "3px",
-    backgroundColor: "hsla(0, 0%, 20%, 0.9)",
-    padding: "7px",
-    marginBottom: "5px",
-    color: "#fff",
-    textAlign: "center",
-    fontSize: "14px",
-  },
-};
-
 function battleReducer(
   state: BattleStateTypes,
   action: BattleAction
